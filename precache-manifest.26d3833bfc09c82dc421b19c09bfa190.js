22bbfef1572e8f5bbbaa22c3f7977ab8598241ae
manan2501 <kevadiyamanan@gmail.com>
2020-01-11 10:39:13 +0530

    Updates

---
+++ precache-manifest.26d3833bfc09c82dc421b19c09bfa190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffc1b3a8f7a2d3bfdd1657aac524120",
+    "url": "/mycalculator/index.html"
+  },
+  {
+    "revision": "b5e48c44e81ee396ecdf",
+    "url": "/mycalculator/static/css/main.a9a6cc8a.chunk.css"
+  },
+  {
+    "revision": "1a9591f603581f4eb2c4",
+    "url": "/mycalculator/static/js/2.12c97329.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mycalculator/static/js/2.12c97329.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5e48c44e81ee396ecdf",
+    "url": "/mycalculator/static/js/main.26664162.chunk.js"
+  },
+  {
+    "revision": "fe1fc31a20baac8d2a99",
+    "url": "/mycalculator/static/js/runtime-main.9e24c0db.js"
+  }
+]);
