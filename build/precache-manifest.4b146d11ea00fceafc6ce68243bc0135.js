f8b2a62b3c335cc0f6c663700db477e4cc65b2bb
lavlife06 <lav7990192369@gmail.com>
2020-01-11 09:27:05 +0530

    package changes

---
+++ build/precache-manifest.4b146d11ea00fceafc6ce68243bc0135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaaed4c1a48dd876b957ed4ec2367282",
+    "url": "/mycalculator/index.html"
+  },
+  {
+    "revision": "1afd55fa9b7801fd3d86",
+    "url": "/mycalculator/static/css/main.a9a6cc8a.chunk.css"
+  },
+  {
+    "revision": "b367814dcfcceff12014",
+    "url": "/mycalculator/static/js/2.22c7efff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mycalculator/static/js/2.22c7efff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1afd55fa9b7801fd3d86",
+    "url": "/mycalculator/static/js/main.d7f3cfd6.chunk.js"
+  },
+  {
+    "revision": "c34ff35870bb4c25b8e3",
+    "url": "/mycalculator/static/js/runtime-main.99d70b91.js"
+  }
+]);
